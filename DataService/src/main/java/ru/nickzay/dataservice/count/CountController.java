3084bcebbe3b94540605789168f8cdd16b82d92c
nickzay <zaitsev.ni@phystech.edu>
2022-12-13 23:42:02 +0300

    work

--- DataService/src/main/java/ru/nickzay/dataservice/count/CountController.java
+++ DataService/src/main/java/ru/nickzay/dataservice/count/CountController.java
@@ -1,13 +1,13 @@
 package ru.nickzay.dataservice.count;
 
-import lombok.RequiredArgsConstructor;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class CountController {
     private final CountService countService;
 
